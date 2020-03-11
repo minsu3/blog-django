3af28aa75a06e4e2cce780097c6f2d35ec632033
Min Su Kim <kim97minsu@gmail.com>
2020-03-10 19:39:24 -0700

    created admin page and database

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 
-posts = [
-  {
-    'author': 'Minsu Kim',
-    'title': 'Blog Post 1',
-    'content': 'First post content',
-    'date_posted': 'March 6, 2020'
-  },
-  {
-    'author': 'Suyoung Min',
-    'title': 'Blog Post 2',
-    'content': 'Second post content',
-    'date_posted': 'March 7, 2018'
-  }
-]
-
 def home(request):
   context = {
-    'posts': posts
+    'posts': Post.objects.all()
   }
   return render(request, 'blog/home.html', context)
 
